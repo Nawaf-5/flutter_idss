de14873ff049e390f35ee1b37d7e2b6fc1888465
Nawaf Aloboudi <aloboudi.n@gmail.com>
2022-12-06 14:28:55 +0300

    new project

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('f'),
+    );
+  }
+}
